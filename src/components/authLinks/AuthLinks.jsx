0bd308163f065eca0599fb59653ca72fd259f7d7
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-02-13 20:52:03 +0700

    Add navbar and context theme

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from './authLinks.module.css';
+export default function AuthLinks() {
+  const status = 'notauthenticated';
+  return (
+    <>
+      {status === 'notauthenticated' ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useContext } from 'react';
+import Image from 'next/image';
+import styles from './themeToggle.module.css';
+import { ThemeContext } from '@/context/ThemeContext';
+
+export default function ThemeToggle() {
+  const { toggle, theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === 'dark'
+          ? { backgroundColor: 'white' }
+          : {
+              backgroundColor: '#0f172a',
+            }
+      }
+    >
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === 'dark'
+            ? { left: 1, background: '#0f172a' }
+            : {
+                right: 1,
+                background: 'white',
+              }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
+}
