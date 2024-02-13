a4f3d51fece1e3603f16e56b53adb0cb167d3595
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-02-13 21:48:16 +0700

    add responsive navbar

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,16 +1,42 @@
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './authLinks.module.css';
 export default function AuthLinks() {
+  const [open, setOpen] = useState(false);
   const status = 'notauthenticated';
   return (
     <>
       {status === 'notauthenticated' ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
           <Link href="/write">Write</Link>
           <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          <span className={styles.link}>Logout</span>
+          {status === 'notauthenticated' ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
