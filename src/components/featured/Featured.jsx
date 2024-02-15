6ec4b7deb5349f1b5ab0df956ad0face595e428c
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-02-15 21:14:31 +0700

    Add featured and categorylist design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,4 +1,29 @@
+import Image from 'next/image';
 import styles from './featured.module.css';
 export default function Featured() {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, onny dev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="featured1" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit{' '}
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
