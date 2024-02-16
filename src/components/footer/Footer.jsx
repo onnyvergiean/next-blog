1c28fa753d2e91997326cce2e69a91519346a8c3
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-02-16 19:57:12 +0700

    add footer,pagination and responsive for both & menu

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,50 @@
+import Link from 'next/link';
 import styles from './footer.module.css';
+import Image from 'next/image';
 export default function Footer() {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>Onnyblog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+          <Image src="/tiktok.png" alt="instagram" width={18} height={18} />
+          <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,4 +1,9 @@
 import styles from './pagination.module.css';
 export default function Pagination() {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.button}>Previous</div>
+      <div className={styles.button}>Next</div>
+    </div>
+  );
 }
